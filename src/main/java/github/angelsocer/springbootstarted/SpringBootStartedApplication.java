696db3c3231003d2395b715257013723df0b6f1f
djay.yim <angelsocer@gmail.com>
2021-08-19 01:16:24 +0900

    [^] startup - builder

--- src/main/java/github/angelsocer/springbootstarted/SpringBootStartedApplication.java
+++ src/main/java/github/angelsocer/springbootstarted/SpringBootStartedApplication.java
@@ -3,13 +3,15 @@ package github.angelsocer.springbootstarted;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootStartedApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(SpringBootStartedApplication.class);
-        application.run(args);
+        new SpringApplicationBuilder()
+                .sources(SpringBootStartedApplication.class)
+                .run(args);
     }
 
 }
